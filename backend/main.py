973750704f62141b3e436950b8a623da87bf9b84
cweim <119029449+cweim@users.noreply.github.com>
2025-06-05 19:22:07 +0800

    deployed!

--- backend/main.py
+++ backend/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://*.vercel.app",
+        "https://weiming-chatbot.vercel.app",
         "https://vercel.app"
     ],
     allow_credentials=True,
